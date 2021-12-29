e2cead0af03c7dbe4917696b0195a1ea4bcb8b0e
Wuliangxl <96579915+Wuliangxl@users.noreply.github.com>
2021-12-29 10:19:14 +0800

    Update snow.js

--- dist/snow.js
+++ dist/snow.js
@@ -10,8 +10,9 @@
 		minSize		: 10,
 		maxSize		: 20,
 		newOn		: 1000,
-		flakeColor	: "#AFDAEF"
-		//flakeColor	: "#AFDAEF" /* 此处可以定义雪花颜色，若要白色可以改为#FFFFFF */
+		flakeColor	: "#AFDAEF" /* 此处可以定义雪花颜色，若要白色可以改为#FFFFFF */
+		//flakeColor	: "#AFDAEF"
+		
 		
 	},
 	    
